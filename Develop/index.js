1376ff07304b325d856f3f3722e5a208588a30e6
Joshua Sands <josh.sands@mail.com>
2021-07-10 18:25:55 -0500

    added readme template and tested writeFile

--- Develop/index.js
+++ Develop/index.js
@@ -1,56 +1,65 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+
 // TODO: Create an array of questions for user input
 
 const promptUser = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'project-title',
-            message: 'What is the title of your project?'
-        },
-        {
-            type: 'input',
-            name: 'project-description',
-            message: 'Describe your project' 
-        },
-        {
-            type: 'input',
-            name: 'project-installation',
-            message: 'What installations are required?' 
-        },
-        {
-            type: 'input',
-            name: 'project-useage',
-            message: 'What is this application used for?' 
-        },
-        {
-            type: 'input',
-            name: 'project-contribution',
-            message: 'Whare are the guidelines for contribution?' 
-        },
-        {
-            type: 'input',
-            name: 'project-test',
-            message: 'Whare are the testing instructions?' 
-        },
-        {
-            type: 'checkbox',
-            name: 'project-license',
-            message: 'Choose your license?',
-            choices: [
-                ['MIT License', 'Other License']
-            ] 
-        },
-        {
-            type: 'input',
-            name: 'project-github',
-            message: 'Enter your GitHub username' 
-        },
-    ])
-}
-promptUser();
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "project_title",
+      message: "What is the title of your project?",
+    },
+    {
+      type: "input",
+      name: "project_description",
+      message: "Describe your project",
+    },
+    {
+      type: "input",
+      name: "project_installation",
+      message: "What installations are required?",
+    },
+    {
+      type: "input",
+      name: "project_useage",
+      message: "What is this application used for?",
+    },
+    {
+      type: "input",
+      name: "project_contribution",
+      message: "Whare are the guidelines for contribution?",
+    },
+    {
+      type: "input",
+      name: "project_test",
+      message: "Whare are the testing instructions?",
+    },
+    {
+      type: "checkbox",
+      name: "project_license",
+      message: "Choose your license?",
+      choices: ["MIT License", "Other License"],
+    },
+    {
+      type: "input",
+      name: "project_github",
+      message: "Enter your GitHub username",
+    },
+  ]);
+};
+promptUser().then((data) => {
+  const readme = generateMarkdown(data);
+
+  return fs.writeFile("../dist/README.md", readme, (err) => {
+    if (err) {
+        console.log(err)
+    }
+    console.log('Your README has been created!')
+    });
+  });
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -13,8 +13,37 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.project_title}
 
+### Table of Contents
+1. [Project Description](#project-description)
+2. [Installation Instructions](#installation-instructions)
+3. [Usage Information](#usage-information)
+4. [Contributor Guidelines](#contributor-guidelines)
+5. [Testing Instructions](#testing-instructions)
+6. [License](#license)
+7. [Follow Me](#follow-me)
+
+## Project Description
+* ${data.project_description}
+
+## Installation Instructions
+* ${data.project_installation}
+
+## Usage Information
+* ${data.project_usage}
+
+## Contributor Guidelines
+* ${data.project_contribution}
+
+## Test Instructions
+* ${data.project_test}
+
+## License
+* licensed under the ${data.project_license}
+
+## Follow Me
+* Follow me on Github at [${data.project_github}](http://github.com/${data.project_github})
 `;
 }
 
